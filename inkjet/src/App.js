d4dffe67a2f017aeecd2d66b2a9c183d9dd278fd
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-29 22:59:51 +0000

    IDE component

--- inkjet/src/App.js
+++ inkjet/src/App.js
@@ -1,43 +1,16 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
-
-import Editor from "@monaco-editor/react";
+import CodeEditor from './components/CodeEditor';
 
 function App() {
-  function handleEditorChange(value, event) {
-    // here is the current value
-  }
-
-  function handleEditorDidMount(editor, monaco) {
-    console.log("onMount: the editor instance:", editor);
-    console.log("onMount: the monaco instance:", monaco);
-  }
-
-  function handleEditorWillMount(monaco) {
-    console.log("beforeMount: the monaco instance:", monaco);
-  }
-
-  function handleEditorValidation(markers) {
-    // model markers
-    // markers.forEach(marker => console.log('onValidate:', marker.message));
-  }
-
   return (
     <div className="application-background">
       <Header />
-      {/* CodeEditor and ChatHistory components will go here when they're created */}
-      <Editor
-      height="90vh"
-      onChange={handleEditorChange}
-      onMount={handleEditorDidMount}
-      beforeMount={handleEditorWillMount}
-      onValidate={handleEditorValidation}
-      defaultLanguage="rust"
-      defaultValue="// some comment"
-    />
+      <CodeEditor />
+      {/* ChatHistory components will go here when it's created */}
     </div>
   );
-};
+}
 
 export default App;
---
+++ inkjet/src/components/CodeEditor.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Editor from "@monaco-editor/react";
+import './CodeEditor.css';
+
+const CodeEditor = () => {
+  function handleEditorChange(value, event) {
+    // Handle the change of the editor's content
+    console.log('Editor content: ', value);
+  }
+
+  function handleEditorDidMount(editor, monaco) {
+    console.log("Editor mounted: the editor instance:", editor);
+    console.log("Editor mounted: the monaco instance:", monaco);
+  }
+
+  function analyzeCode() {
+    // Trigger analysis on the code currently in the editor
+    console.log('Analyze button clicked');
+  }
+
+  return (
+    <div className="code-editor-container">
+      <h2 className="editor-title">Smart Contract Code</h2>
+      <Editor
+        height="50vh"
+        onChange={handleEditorChange}
+        onMount={handleEditorDidMount}
+        defaultLanguage="rust"
+        defaultValue="// Example ink! smart contract code"
+        options={{ automaticLayout: true }}
+      />
+      <div className="editor-footer">
+        <div className="editor-templates">
+          <label htmlFor="templates" className="templates-label">Templates:</label>
+          <select id="templates" className="templates-select">
+            <option>Template 1</option>
+            <option>Template 2</option>
+            <option>Template 3</option>
+          </select>
+        </div>
+        <button className="analyze-button" onClick={analyzeCode}>
+          Analyze
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CodeEditor;
