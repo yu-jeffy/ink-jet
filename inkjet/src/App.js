2c0559ae0d00e97c31e0feff04580fc4e16e0570
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-29 22:56:31 +0000

    header, components files

--- inkjet/src/App.js
+++ inkjet/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from './components/Header';
+
+import Editor from "@monaco-editor/react";
 
 function App() {
+  function handleEditorChange(value, event) {
+    // here is the current value
+  }
+
+  function handleEditorDidMount(editor, monaco) {
+    console.log("onMount: the editor instance:", editor);
+    console.log("onMount: the monaco instance:", monaco);
+  }
+
+  function handleEditorWillMount(monaco) {
+    console.log("beforeMount: the monaco instance:", monaco);
+  }
+
+  function handleEditorValidation(markers) {
+    // model markers
+    // markers.forEach(marker => console.log('onValidate:', marker.message));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="application-background">
+      <Header />
+      {/* CodeEditor and ChatHistory components will go here when they're created */}
+      <Editor
+      height="90vh"
+      onChange={handleEditorChange}
+      onMount={handleEditorDidMount}
+      beforeMount={handleEditorWillMount}
+      onValidate={handleEditorValidation}
+      defaultLanguage="rust"
+      defaultValue="// some comment"
+    />
     </div>
   );
-}
+};
 
 export default App;
